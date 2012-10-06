66cc9d2613ad2a5ff106a04b91bb816e935de8d0
Alex Wright <alex@xeentech.com>
2012-10-06 16:40:04 +0100

    Basics of oAuth. just a public static to sign a request instance.

---
+++ src/com/xeentech/tent/client/OAuth2.java
@@ -0,0 +1,69 @@
+package com.xeentech.tent.client;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.http.HttpRequest;
+
+import android.net.Uri;
+import android.util.Base64;
+import android.util.Log;
+
+public class OAuth2 {
+	public static void sign (HttpRequest request, String keyId, String keySecret) {
+		Uri uri = Uri.parse(request.getRequestLine().getUri());
+		int port = uri.getPort() > 0 ? uri.getPort() : (uri.getScheme().equals("https") ? 443 : 80); 
+		
+		Long ts = generateTs();
+		String nonce = generateNonce();
+		
+		StringBuilder sb = new StringBuilder();
+		sb
+			.append(generateTs().toString()).append("\n")
+			.append(nonce).append("\n")
+			.append(request.getRequestLine().getMethod()).append("\n")
+			.append(uri.getPath()).append("\n")
+			.append(uri.getHost()).append("\n")
+			.append(port).append("\n")
+			.append("").append("\n"); // empty ext field;
+		
+		Log.d(OAuth2.class.toString(), "sign(): " + sb.toString());
+		String sigBase = sb.toString();
+		String mac = generateMac(sigBase, keySecret);
+		
+		String macFormat = "MAC id=\"%s\", ts=\"%s\", nonce=\"%s\", mac=\"%s\"";
+		String authHeader = String.format(macFormat, keyId, ts.toString(), nonce, mac);
+		Log.d(OAuth2.class.toString(), "sign(): auth: " + authHeader);
+		Log.d(OAuth2.class.toString(), "sign(): secret: " + keySecret);
+		request.addHeader("Authorization", authHeader);
+	}
+	
+	protected static Long generateTs () {
+		int startAt = 1262304000; // 2010-01-01T00:00:00Z
+		return (System.currentTimeMillis() / 1000) - startAt;
+	}
+	
+	protected static String generateNonce () {
+		Long nonce = new Random().nextLong();
+		return Long.toHexString(nonce);
+	}
+	
+	protected static String generateMac (String base, String key) {
+		try {
+			SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "HmacSHA256");
+			Mac mac = Mac.getInstance("HmacSHA256");
+			mac.init(keySpec);
+		
+			return Base64.encodeToString(mac.doFinal(base.getBytes()), Base64.NO_WRAP);
+		}
+		catch (NoSuchAlgorithmException e) {
+		}
+		catch (InvalidKeyException e) {
+		}
+		return null;
+	}
+}
